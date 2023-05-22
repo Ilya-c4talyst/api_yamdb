096ebd9403f3feea5def80a0182dbacd54f2e2bb
Алина Урсова <alina.ursova@yandex.ru>
2023-05-22 16:47:40 +0300

    правки

--- api_yamdb/api/v1/views.py
+++ api_yamdb/api/v1/views.py
@@ -143,50 +143,30 @@ class ReviewViewSet(viewsets.ModelViewSet):
                           IsAuthenticatedOrReadOnly)
     http_method_names = ["patch", "get", "post", "delete"]
 
+    def get_title(self):
+        return get_object_or_404(Title, pk=self.kwargs["title_id"])
+
     def get_queryset(self):
-        title = get_object_or_404(Title, pk=self.kwargs["title_id"])
-        return title.review.all()
+        return self.get_title().review.all()
 
     def perform_create(self, serializer):
-        title = get_object_or_404(Title, pk=self.kwargs["title_id"])
-        serializer.save(author=self.request.user, title=title)
-
-    def perform_update(self, serializer):
-        review = self.get_object()
-        if not (
-            self.request.user == review.author
-            or self.request.user.is_staff
-            or self.request.user.is_moderator
-            or self.request.user.is_admin
-        ):
-            return Response(status=status.HTTP_400_BAD_REQUEST)
-        serializer.save()
+        serializer.save(author=self.request.user, title=self.get_title())
 
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = (IsAuthorModeratorAdminOrReadOnly,
                           IsAuthenticatedOrReadOnly)
+    http_method_names = ["patch", "get", "post", "delete"]
 
-    def get_queryset(self):
-        review = get_object_or_404(
+    def get_review(self):
+        return get_object_or_404(
             Review, pk=self.kwargs["review_id"], title=self.kwargs["title_id"]
         )
-        return review.comment.all()
+    
+    def get_queryset(self):
+        return self.get_review().comment.all()
 
     def perform_create(self, serializer):
-        review = get_object_or_404(
-            Review, pk=self.kwargs["review_id"], title=self.kwargs["title_id"]
-        )
-        serializer.save(author=self.request.user, review=review)
+        serializer.save(author=self.request.user, review=self.get_review())
 
-    def perform_update(self, serializer):
-        comment = self.get_object()
-        if not (
-            self.request.user == comment.author
-            or self.request.user.is_staff
-            or self.request.user.is_moderator
-            or self.request.user.is_admin
-        ):
-            return Response(status=status.HTTP_400_BAD_REQUEST)
-        serializer.save()
